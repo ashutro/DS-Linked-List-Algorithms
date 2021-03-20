de6a84e37a7f13dde612de556cc345dd8f94bf80
ashutro <ashutoshplz99@gmail.com>
2021-03-21 00:20:35 +0530

    minor update

--- ListCreate.c
+++ ListCreate.c
@@ -1,3 +1,6 @@
+ /*
+    #create linked-list
+*/
 #include<stdio.h>
 #include<stdlib.h>
 struct Node
@@ -9,40 +12,47 @@ void create(int n)
 {
     int i,num;
     struct Node *temp,*head;
-    first = (struct Node *)malloc(sizeof(struct Node ));
-    if (first == NULL)
+    if(n > 0)
     {
-        printf("Not Allocated!!!");
-        exit -1;
-    }
-    else
-    {
-        printf("Enter data: 1 ");
-        scanf("%d",&num);
-        first->data=num;
-        first->next=NULL;
-        head  = first;
-    }
-
-    for ( i = 2; i <= n; i++)
-    {
-        temp = (struct Node *)malloc(sizeof(struct Node));
-        if (temp == NULL)
+        first = (struct Node *)malloc(sizeof(struct Node ));
+        if (first == NULL)
         {
             printf("Not Allocated!!!");
             exit -1;
         }
-        else
+        else 
         {
-            printf("Enter data: %d ",i);
+            
+            printf("Enter data: 1 ");
             scanf("%d",&num);
-            temp->data = num;
-            temp->next = NULL;
-            head->next =temp;
-            head = temp;
+            first->data=num;
+            first->next=NULL;
+            head  = first;
+            for ( i = 2; i <= n; i++)
+            {
+                temp = (struct Node *)malloc(sizeof(struct Node));
+                if (temp == NULL)
+                {
+                    printf("Not Allocated!!!");
+                    exit -1;
+                }
+                else
+                {
+                    printf("Enter data: %d ",i);
+                    scanf("%d",&num);
+                    temp->data = num;
+                    temp->next = NULL;
+                    head->next =temp;
+                    head = temp;
+                }
+                
+                
+            }
         }
-        
-        
+    }
+    else 
+    {
+        printf("size is zero");
     }
 }
 void Display()
