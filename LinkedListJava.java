23f5293e5fe646ae93fb0f578d664f2714d2bbce
Ashutosh Kumar <unknwnmex@gmail.com>
2022-03-16 02:18:16 +0530

    Creating LinkedList in java

---
+++ LinkedListJava.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class LinkedListJava{
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        LinkedList<String>  ListString = 
+            new LinkedList<String>();
+
+            ListString.add("A");
+            ListString.add("B");
+            ListString.add("C");
+
+            System.out.println(ListString);
+    }
+}
